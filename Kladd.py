eaea738630a078ed5c2849f94ca443594c98d759
Barwago <Barwago_m@hotmail.com>
2021-10-27 18:46:11 +0200

    hh

---
+++ Kladd.py
@@ -0,0 +1,16 @@
+
+# To create the board, we use matrices to represent each square
+
+
+import numpy as np 
+
+#A function for the board
+def creat_board():
+#Matrix with zeros 7x6
+  board = np.zeros(6,7)
+  return board
+
+board = creat_board()
+print (board)
+
+
