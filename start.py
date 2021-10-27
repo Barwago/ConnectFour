82763e264628b66c170a5e699bf249884f6d34b7
sheima <s338853@oslomet.no>
2021-10-27 18:45:34 +0200

    sss

--- start.py
+++ start.py
@@ -1,1 +1,11 @@
-#numpy insstalert 
+# To create the board, we use matrices to represent each square
+import numpy as np 
+
+#A function for the board
+def creat_board():
+#Matrix with zeros 7x6
+  board = np.zeros((6,7))
+  return board
+
+board = creat_board()
+print (board)
